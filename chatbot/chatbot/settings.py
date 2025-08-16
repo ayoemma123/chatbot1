a3ffa79c9eb3d5b72aea5aa2e917bae1a657decb
Olulana Emmanuel <emmanuelolulana72@gmail.com>
2025-08-16 19:44:36 +0100

    Prod Test

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -9,23 +9,40 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Support env variables from .env file if defined
+from dotenv import load_dotenv
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
+load_dotenv(env_path)
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-um3k8i%--pcluneokrlcnz+wbu59p--ads3^5qoa7+q03u1h2j'
+#SECRET_KEY = 'django-insecure-um3k8i%--pcluneokrlcnz+wbu59p--ads3^5qoa7+q03u1h2j'
+
+SECRET_KEY = "3lp5wji$1ah39s1f(r1vymv(680d6(2o@#lqt()@%8op5sv%#f"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = False
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 
 # Application definition
